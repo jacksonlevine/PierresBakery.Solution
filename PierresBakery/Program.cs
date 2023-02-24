660b03e055ca8c8cf374d32b627a9cf6ada1b521
jacksonlevine <jack@netbug.us>
2023-02-24 09:40:43 -0800

    WIP: start writing UI

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -1,10 +1,13 @@
+using PierresBakery.Business;
+using System;
 namespace PierresBakery 
 {
   public class Program 
   {
     static void Main()
     {
-      
+      ShoppingCart myCart = new ShoppingCart();
+      Console.WriteLine("Hello! Welcome to Pierre's Bakery. ");
     }
   }
 }
