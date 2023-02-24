fa241093dbaa6cf71197561f7d48e8903e505700
jacksonlevine <jack@netbug.us>
2023-02-24 08:17:17 -0800

    write second test

--- PierresBakery.Tests/BusinessTests/BakeryTests.cs
+++ PierresBakery.Tests/BusinessTests/BakeryTests.cs
@@ -12,5 +12,13 @@ namespace PierresBakery.Tests
       Bread b = new Bread();
       Assert.AreEqual(typeof(Bread), b.GetType());
     }
+
+    [TestMethod]
+
+    public void Pastry_CreatesInstanceOfPastry_Pastry()
+    {
+      Pastry p = new Pastry();
+      Assert.AreEqual(typeof(Pastry), p.GetType());
+    }
   }
 }
