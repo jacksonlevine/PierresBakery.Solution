9b2a75a2c08be277b84b99bdd2fd7fe7bdbc12a2
jacksonlevine <jack@netbug.us>
2023-02-24 08:08:44 -0800

    add files to project

---
+++ PierresBakery.Tests/BusinessTests/BakeryTests.cs
@@ -0,0 +1,11 @@
+using PierresBakery.Business;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PierresBakery.Tests 
+{
+  [TestClass]
+  public class BakeryTests
+  {
+    
+  }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Metro2D
+## A game using the .NET framework and test-driven development with unit tests.
+### By Jackson Levine
+# Goals:
+* TBD
+
