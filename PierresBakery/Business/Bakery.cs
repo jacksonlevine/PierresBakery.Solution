9b03707924f858d586e4684f27834d46f135e1f9
jacksonlevine <jack@netbug.us>
2023-02-24 08:57:37 -0800

    write code to pass shopping cart calculatetotal test

--- PierresBakery/Business/Bakery.cs
+++ PierresBakery/Business/Bakery.cs
@@ -1,12 +1,27 @@
 using System;
+using System.Collections.Generic;
 namespace PierresBakery.Business 
 {
   public class ShoppingCart
   {
-    
+    public List<IBakeryItem> contents = new List<IBakeryItem>();
+    public void AddToCart(IBakeryItem b)
+    {
+      contents.Add(b);
+    }
+
+    public double CalculateTotal()
+    {
+      double price = 0.0;
+      foreach(IBakeryItem b in contents) 
+      {
+        price += b.Price;
+      }
+      return price;
+    }
   }
 
-  interface IBakeryItem
+  public interface IBakeryItem
   {
     public double Price{ get; set; }
     public void MakeFree();
