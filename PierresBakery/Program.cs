615872163e0797cfe2c4e3fde88b0cae58db83e0
jacksonlevine <jack@netbug.us>
2023-02-24 11:39:59 -0800

    add two new commands to quit the app or clear your cart

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -15,6 +15,10 @@ namespace PierresBakery
         string[] nlSplit = nextLine.Split(' ');
         switch(nlSplit[0])
         {
+          case "clearcart":
+            myCart.contents.Clear();
+            Console.WriteLine("Successfully cleared your shopping cart.");
+            break;
           case "viewcart":
             displayCart(myCart);
             break;
@@ -47,6 +51,9 @@ namespace PierresBakery
           case "help":
             help();
             break;
+          case "quit":
+            isEnd = true;
+            break;
           default:
             Console.WriteLine("Unknown command! Type help to view the list of commands.");
             break;
@@ -57,10 +64,12 @@ namespace PierresBakery
     static void help() 
     {
       Console.WriteLine("Hello! Welcome to Pierre's Bakery. Here's a list of commands:");
+      Console.WriteLine("clearcart - Empty your shopping cart!");
       Console.WriteLine("viewcart - View the contents of your shopping cart!");
       Console.WriteLine("buy bread <amount> - Example: buy bread 1 - Add bread to your cart. Every third bread is free!");
       Console.WriteLine("buy pastry <amount> - Example: buy pastry 2 - Add pastries to your cart. Every fourth pastry is free!");
       Console.WriteLine("help - View this list of commands :)");
+      Console.WriteLine("quit - Leave the bakery");
     }
 
     static void displayCart(ShoppingCart myCart)
@@ -81,7 +90,8 @@ namespace PierresBakery
         for(int i = -leftPadding; i < width; i++)
         {
           bool onLine = false;
-          if(i >= 0 && i < theLine.Length) {
+          if(i >= 0 && i < theLine.Length)
+          {
             onLine = true;
           }
           if(j == -topBottomPadding || j == myCart.contents.Count + topBottomPadding -1 || i == -leftPadding || i == width - 1)
