2ad7baec2a359645784d3cd4d2e0c5b8d48b1383
jacksonlevine <jack@netbug.us>
2023-02-24 11:27:56 -0800

    add new UI for accepting multiple argument commands

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -12,18 +12,35 @@ namespace PierresBakery
       while(!isEnd)
       {
         string nextLine = Console.ReadLine();
-        switch(nextLine)
+        string[] nlSplit = nextLine.Split(' ');
+        switch(nlSplit[0])
         {
           case "viewcart":
             displayCart(myCart);
             break;
-          case "buy bread":
-            myCart.AddToCart(new Bread());
-            Console.WriteLine("You bought <1> of Bread!");
-            break;
-          case "buy pastry":
-            myCart.AddToCart(new Pastry());
-            Console.WriteLine("You bought <1> of Pastry!");
+          case "buy":
+            try 
+            {
+              switch(nlSplit[1]) 
+              {
+                case "bread":
+                  for(int i = 0; i < int.Parse(nlSplit[2]); i++)
+                  {
+                    myCart.AddToCart(new Bread());
+                  }
+                  break;
+                case "pastry":
+                  for(int i = 0; i < int.Parse(nlSplit[2]); i++)
+                  {
+                    myCart.AddToCart(new Pastry());
+                  }
+                  break;
+              }
+            }
+            catch(Exception e)
+            {
+              Console.WriteLine("Too few arguments! Please type help for command formats. " + e.Message);
+            }
             break;
           case "help":
             help();
@@ -39,8 +56,8 @@ namespace PierresBakery
     {
       Console.WriteLine("Hello! Welcome to Pierre's Bakery. Here's a list of commands:");
       Console.WriteLine("viewcart - View the contents of your shopping cart!");
-      Console.WriteLine("buy bread - Add bread to your cart. Every third bread is free!");
-      Console.WriteLine("buy pastry - Add one pastry to your cart. Every second pastry is free!");
+      Console.WriteLine("buy bread <amount> - Example: buy bread 1 - Add bread to your cart. Every third bread is free!");
+      Console.WriteLine("buy pastry <amount> - Example: buy pastry 2 - Add pastries to your cart. Every fourth pastry is free!");
       Console.WriteLine("help - View this list of commands :)");
     }
 
